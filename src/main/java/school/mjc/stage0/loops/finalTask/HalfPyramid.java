215a97b5c1cdeff1c219b4dbe3cb6e93edee1b25
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-03 10:03:59 +0300

    Only 1 task completed.

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        char symbol = '*';
+        for(int i = 1;i <= cathetusLength;i++) {
+            for (int y = 1; y <= i; y++) {
+                System.out.println(symbol);
+            }
+            System.out.println("");
+        }
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
+    int result = 0;
     public void printTable(int numberTableToPrint){
+        for(int i = 1;i <= 10;i++){
+            result = i * numberTableToPrint;
+            System.out.println(i+ " x " + numberTableToPrint + " = " + result );
+        }
     }
 }
